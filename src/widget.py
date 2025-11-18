c68757737add430450c6acb357178eb54d32c25c
Vadim Semenov <vadimsemenov53@gmail.com>
2025-11-18 19:33:55 +0300

    clear code black, isort, flake8, mypy

--- src/widget.py
+++ src/widget.py
@@ -32,4 +32,4 @@ def mask_account_card(account_card: str) -> str:
 def get_date(date: str) -> str:
     """Функция возвращает дату из принятой строки
     (Формат строки -> "2024-03-11T02:26:18.671407")"""
-    return date[8:10] + '.' + date[5:7] + '.' + date[:4] + ' (ДД.ММ.ГГГГ)'
+    return date[8:10] + "." + date[5:7] + "." + date[:4] + " (ДД.ММ.ГГГГ)"
--- test/test_main.py
+++ test/test_main.py
@@ -1,10 +1,10 @@
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 help(mask_account_card)
 print(mask_account_card("Счет 13312445665"))
 print(mask_account_card("Visa Platinum 2342342345878799"))
 print(mask_account_card("Maestro 1596837868705199"))
-print('')
+print("")
 
 help(get_date)
 print(get_date("2024-03-11T02:26:18.671407"))
