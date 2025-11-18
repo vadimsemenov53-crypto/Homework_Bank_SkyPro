5971baf1025df1615a6e1784795b0a760cd71868
Vadim Semenov <vadimsemenov53@gmail.com>
2025-11-18 19:30:52 +0300

    add func get_date

--- src/widget.py
+++ src/widget.py
@@ -7,7 +7,7 @@ def mask_account_card(account_card: str) -> str:
     """Функция возвращает строку с замаскированным номером карты или счет"""
     account = ""
     pattern = "Счет"
-    # match = re.search(pattern, account_card)
+
     if re.search(pattern, account_card):
         for element in account_card:
             if element.isdigit():
@@ -27,3 +27,9 @@ def mask_account_card(account_card: str) -> str:
 
     else:
         return "Информация о счете отсутствует"
+
+
+def get_date(date: str) -> str:
+    """Функция возвращает дату из принятой строки
+    (Формат строки -> "2024-03-11T02:26:18.671407")"""
+    return date[8:10] + '.' + date[5:7] + '.' + date[:4] + ' (ДД.ММ.ГГГГ)'
--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,11 @@
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_date
 
 help(mask_account_card)
 print(mask_account_card("Счет 13312445665"))
 print(mask_account_card("Visa Platinum 2342342345878799"))
+print(mask_account_card("Maestro 1596837868705199"))
+print('')
+
+help(get_date)
+print(get_date("2024-03-11T02:26:18.671407"))
+print(get_date("2021-12-25T03:29:11.883052"))
