70e685368ee17b3d1d7f7977e3a459565fe7d976
Vadim Semenov <vadimsemenov53@gmail.com>
2025-11-25 15:02:42 +0300

    testing func filter_by_state

--- test/test_main.py
+++ test/test_main.py
@@ -1,11 +1,10 @@
-from src.widget import get_date, mask_account_card
+from src.processing import filter_by_state
 
-help(mask_account_card)
-print(mask_account_card("Счет 13312445665"))
-print(mask_account_card("Visa Platinum 2342342345878799"))
-print(mask_account_card("Maestro 1596837868705199"))
-print("")
+info_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-help(get_date)
-print(get_date("2024-03-11T02:26:18.671407"))
-print(get_date("2021-12-25T03:29:11.883052"))
+resualt_filter = filter_by_state(info_list, state='CANCELED')
+for element in resualt_filter:
+    print(element)
