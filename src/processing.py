d8ff0c46bc37d5a1cc9e1047c4d1ac2df832fd22
Vadim Semenov <vadimsemenov53@gmail.com>
2025-11-25 14:57:04 +0300

    add func filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,10 @@
+def filter_by_state(data_list: list, state='EXECUTED') -> list:
+    """Функция фильтрует полученные данные по ключу state
+    (по умолчанию state='EXECUTED')"""
+    filtered_list = []
+
+    for info in data_list:
+        if info['state'] == state:
+            filtered_list.append(info)
+
+    return filtered_list
