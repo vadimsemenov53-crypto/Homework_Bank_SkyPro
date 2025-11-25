70874c6094aa0bc6a0798f2a0a721e59610ee44d
Vadim Semenov <vadimsemenov53@gmail.com>
2025-11-25 15:01:22 +0300

    clear code flake8, mypy, isort, black processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,10 @@
-def filter_by_state(data_list: list, state='EXECUTED') -> list:
+def filter_by_state(data_list: list, state: str = "EXECUTED") -> list:
     """Функция фильтрует полученные данные по ключу state
     (по умолчанию state='EXECUTED')"""
     filtered_list = []
 
     for info in data_list:
-        if info['state'] == state:
+        if info["state"] == state:
             filtered_list.append(info)
 
     return filtered_list
