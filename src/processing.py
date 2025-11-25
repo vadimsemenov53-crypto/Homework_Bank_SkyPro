a04ca953a2fd30985f083a68709f1dfad2fe08cb
Vadim Semenov <vadimsemenov53@gmail.com>
2025-11-25 16:42:33 +0300

    fixes func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -10,11 +10,12 @@ def filter_by_state(data_list: list, state: str = "EXECUTED") -> list:
     return filtered_list
 
 
-def sort_by_date(data_list: list, sort: str = "decreasing") -> list:
+def sort_by_date(data_list: list, sort: bool = True) -> list:
     """Функция сортирует полученные данные по дате
     принимая список словарей и параметр сортировки,
-    возвращает отсортированный список словарей"""
-    if sort == "decreasing":
+    возвращает отсортированный список словарей.
+    Сортировка по умолчанию - убывание (sort = True)"""
+    if sort:
         sorted_list = sorted(data_list, key=lambda info: info["date"][:10], reverse=True)
         return sorted_list
     else:
