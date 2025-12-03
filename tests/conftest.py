6a47ddd7bb2bb9db85522483c62d2cbe35bbb756
Vadim Semenov <vadimsemenov53@gmail.com>
2025-12-03 19:52:08 +0300

    clear code test_masks.py, conftest.py -> flake8, mypy, isort, black

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,9 @@ import pytest
 
 @pytest.fixture()
 def card_number():
-    return '2202001366664707'
+    return "2202001366664707"
 
 
 @pytest.fixture()
 def number_account():
-    return '271728141201'
+    return "271728141201"
