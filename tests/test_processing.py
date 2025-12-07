8b05752f22815a36852e2f5ef2a5d5f9cf27625d
Vadim Semenov <vadimsemenov53@gmail.com>
2025-12-07 15:28:13 +0300

    clear code test_processing.py -> black isort flake8 mypy

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -18,60 +18,71 @@ def test_filter_by_state_wrong_type():
     with pytest.raises(TypeError):
         filter_by_state([123356])
     with pytest.raises(TypeError):
-        filter_by_state(['error'])
+        filter_by_state(["error"])
 
 
 def test_filter_by_state_not_key():
     with pytest.raises(KeyError):
-        filter_by_state([
-            {"id": 939719570, "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "date": "2018-09-12T21:27:25.241689"}])
+        filter_by_state(
+            [
+                {"id": 939719570, "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
+            ]
+        )
 
 
-@pytest.mark.parametrize('my_list, expected', [([
-    {"state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
-    {"state": "CANCELED", "date": "2018-06-30T02:08:58.425572"}
-                                                ],
-     [
-         {'date': '2019-07-03T18:35:29.512364', 'state': 'CANCELED'},
-         {'date': '2018-06-30T02:08:58.425572', 'state': 'CANCELED'}
-     ]),
-    ([
-        {"id": 594226727, "state": "CANCELED"},
-        {"id": 615064591, "state": "CANCELED"}],
-     [{'id': 594226727, 'state': 'CANCELED'},
-     {'id': 615064591, 'state': 'CANCELED'}]
-    )
-])
+@pytest.mark.parametrize(
+    "my_list, expected",
+    [
+        (
+            [
+                {"state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+                {"state": "CANCELED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+            [
+                {"date": "2019-07-03T18:35:29.512364", "state": "CANCELED"},
+                {"date": "2018-06-30T02:08:58.425572", "state": "CANCELED"},
+            ],
+        ),
+        (
+            [{"id": 594226727, "state": "CANCELED"}, {"id": 615064591, "state": "CANCELED"}],
+            [{"id": 594226727, "state": "CANCELED"}, {"id": 615064591, "state": "CANCELED"}],
+        ),
+    ],
+)
 def test_filter_by_state_another_list(my_list, expected):
-    assert filter_by_state(my_list, state='CANCELED') == expected
+    assert filter_by_state(my_list, state="CANCELED") == expected
+
 
 def test_sort_by_date_base(processing):
     assert sort_by_date(processing) == [
-        {'date': '2019-07-03T18:35:29.512364', 'id': 41428829, 'state': 'EXECUTED'},
-        {'date': '2018-10-14T08:21:33.419441', 'id': 615064591, 'state': 'CANCELED'},
-        {'date': '2018-09-12T21:27:25.241689', 'id': 594226727, 'state': 'CANCELED'},
-        {'date': '2018-06-30T02:08:58.425572', 'id': 939719570, 'state': 'EXECUTED'}]
+        {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
+        {"date": "2018-10-14T08:21:33.419441", "id": 615064591, "state": "CANCELED"},
+        {"date": "2018-09-12T21:27:25.241689", "id": 594226727, "state": "CANCELED"},
+        {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
+    ]
 
     assert sort_by_date(processing, sort=False) == [
-        {'date': '2018-06-30T02:08:58.425572', 'id': 939719570, 'state': 'EXECUTED'},
-        {'date': '2018-09-12T21:27:25.241689', 'id': 594226727, 'state': 'CANCELED'},
-        {'date': '2018-10-14T08:21:33.419441', 'id': 615064591, 'state': 'CANCELED'},
-        {'date': '2019-07-03T18:35:29.512364', 'id': 41428829, 'state': 'EXECUTED'}
+        {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
+        {"date": "2018-09-12T21:27:25.241689", "id": 594226727, "state": "CANCELED"},
+        {"date": "2018-10-14T08:21:33.419441", "id": 615064591, "state": "CANCELED"},
+        {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
     ]
 
 
 def test_sort_by_date_another_date():
-    assert sort_by_date([
-        {'date': '2032-01-30T02:08:58.425572', 'id': 939719570, 'state': 'EXECUTED'},
-        {'date': '1900-02-12T21:27:25.241689', 'id': 594226727, 'state': 'CANCELED'}], sort=False) == [
-        {'date': '1900-02-12T21:27:25.241689', 'id': 594226727, 'state': 'CANCELED'},
-        {'date': '2032-01-30T02:08:58.425572', 'id': 939719570, 'state': 'EXECUTED'}
+    assert sort_by_date(
+        [
+            {"date": "2032-01-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
+            {"date": "1900-02-12T21:27:25.241689", "id": 594226727, "state": "CANCELED"},
+        ],
+        sort=False,
+    ) == [
+        {"date": "1900-02-12T21:27:25.241689", "id": 594226727, "state": "CANCELED"},
+        {"date": "2032-01-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
     ]
 
 
 def test_sort_by_date_not_key():
     with pytest.raises(KeyError):
-        sort_by_date([
-            {"id": 594226727, "state": "CANCELED"},
-            {"id": 615064591, "state": "CANCELED"}])
+        sort_by_date([{"id": 594226727, "state": "CANCELED"}, {"id": 615064591, "state": "CANCELED"}])
