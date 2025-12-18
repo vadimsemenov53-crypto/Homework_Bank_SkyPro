34bee0e0ab9ab20f7f1a3102e37ba6aa041424c0
Vadim Semenov <vadimsemenov53@gmail.com>
2025-12-18 17:55:46 +0300

    clear code black, mypy, isort, flake8

--- src/generators.py
+++ src/generators.py
@@ -5,7 +5,7 @@ def filter_by_currency(transaction_data: list[dict], currency: str) -> Generator
     """Функция принимает на вход список словарей, представляющих транзакции.
     Поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD)."""
     for index in range(len(transaction_data)):
-        if transaction_data[index]["operationAmount"]['currency']['code'] == currency.upper():
+        if transaction_data[index]["operationAmount"]["currency"]["code"] == currency.upper():
             yield transaction_data[index]
 
 
@@ -15,20 +15,20 @@ def transaction_descriptions(transaction_data: list[dict]) -> Generator[str]:
         for data in transaction_data:
             yield data["description"]
     else:
-        yield 'Нет данных'
+        yield "Нет данных"
 
 
 def card_number_generator(start_gen: int, stop_gen: int) -> Generator[str]:
     """Генератор, который выдает номера банковских карт в формате
-     ( XXXX XXXX XXXX XXXX ), где X — цифра номера карты.
-     Генератор может сгенерировать номера карт в заданном диапазоне
-      от 0000 0000 0000 0001 до 9999 9999 9999 9999."""
+    ( XXXX XXXX XXXX XXXX ), где X — цифра номера карты.
+    Генератор может сгенерировать номера карт в заданном диапазоне
+     от 0000 0000 0000 0001 до 9999 9999 9999 9999."""
     if start_gen > stop_gen:
-        yield f'Заданы неверные параметры start > stop: {start_gen} > {stop_gen}'
+        yield f"Заданы неверные параметры start > stop: {start_gen} > {stop_gen}"
     else:
-        for num in range(start_gen, stop_gen+1):
+        for num in range(start_gen, stop_gen + 1):
             if num < 10000000000000000:
                 card_number = str(num).zfill(16)
-                yield f'{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}'
+                yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
             else:
-                yield 'Достигнуто крайнее значение: 9999 9999 9999 9999'
+                yield "Достигнуто крайнее значение: 9999 9999 9999 9999"
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,158 +1,157 @@
 import pytest
 
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
+
 
 def test_filter_by_currency_base(generators_data):
-    gen = filter_by_currency(generators_data, 'USD')
+    gen = filter_by_currency(generators_data, "USD")
 
-    assert next(gen) == {'date': '2018-06-30T02:08:58.425572',
- 'description': 'Перевод организации',
- 'from': 'Счет 75106830613657916952',
- 'id': 939719570,
- 'operationAmount': {'amount': '9824.07',
-                     'currency': {'code': 'USD', 'name': 'USD'}},
- 'state': 'EXECUTED',
- 'to': 'Счет 11776614605963066702'}
+    assert next(gen) == {
+        "date": "2018-06-30T02:08:58.425572",
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "id": 939719570,
+        "operationAmount": {"amount": "9824.07", "currency": {"code": "USD", "name": "USD"}},
+        "state": "EXECUTED",
+        "to": "Счет 11776614605963066702",
+    }
 
-    assert next(gen) == {'date': '2019-04-04T23:20:05.206878',
- 'description': 'Перевод со счета на счет',
- 'from': 'Счет 19708645243227258542',
- 'id': 142264268,
- 'operationAmount': {'amount': '79114.93',
-                     'currency': {'code': 'USD', 'name': 'USD'}},
- 'state': 'EXECUTED',
- 'to': 'Счет 75651667383060284188'}
+    assert next(gen) == {
+        "date": "2019-04-04T23:20:05.206878",
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "id": 142264268,
+        "operationAmount": {"amount": "79114.93", "currency": {"code": "USD", "name": "USD"}},
+        "state": "EXECUTED",
+        "to": "Счет 75651667383060284188",
+    }
 
-    assert next(gen) == {'date': '2018-08-19T04:27:37.904916',
- 'description': 'Перевод с карты на карту',
- 'from': 'Visa Classic 6831982476737658',
- 'id': 895315941,
- 'operationAmount': {'amount': '56883.54',
-                     'currency': {'code': 'USD', 'name': 'USD'}},
- 'state': 'EXECUTED',
- 'to': 'Visa Platinum 8990922113665229'}
+    assert next(gen) == {
+        "date": "2018-08-19T04:27:37.904916",
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "id": 895315941,
+        "operationAmount": {"amount": "56883.54", "currency": {"code": "USD", "name": "USD"}},
+        "state": "EXECUTED",
+        "to": "Visa Platinum 8990922113665229",
+    }
 
     with pytest.raises(StopIteration):
         assert next(gen)
 
-@pytest.mark.parametrize('transactions_info, currency, expected',
-[
-    (
+
+@pytest.mark.parametrize(
+    "transactions_info, currency, expected",
+    [
+        (
             [
-                {"operationAmount": {"currency": {"code": "EUR"}},
-                 "description": "Перевод организации"},
-                {"operationAmount": {"currency": {"code": "USD"}},
-                 "description": "Перевод с карты на карту"},
-                {"operationAmount": {"currency": {"code": "EUR"}},
-                 "description": "Анонимный перевод"}
+                {"operationAmount": {"currency": {"code": "EUR"}}, "description": "Перевод организации"},
+                {"operationAmount": {"currency": {"code": "USD"}}, "description": "Перевод с карты на карту"},
+                {"operationAmount": {"currency": {"code": "EUR"}}, "description": "Анонимный перевод"},
             ],
-            'EUR',
-            'Перевод организации, Перевод с карты на карту, Анонимный перевод'
-    )
-])
+            "EUR",
+            "Перевод организации, Перевод с карты на карту, Анонимный перевод",
+        )
+    ],
+)
 def test_filter_by_currency_another_data(transactions_info, currency, expected):
     gen = filter_by_currency(transactions_info, currency)
-    assert next(gen) == {'operationAmount': {'currency': {'code': 'EUR'}}, 'description': 'Перевод организации'}
-    assert next(gen) == {'operationAmount': {'currency': {'code': 'EUR'}}, 'description': 'Анонимный перевод'}
+    assert next(gen) == {"operationAmount": {"currency": {"code": "EUR"}}, "description": "Перевод организации"}
+    assert next(gen) == {"operationAmount": {"currency": {"code": "EUR"}}, "description": "Анонимный перевод"}
     with pytest.raises(StopIteration):
         assert next(gen)
 
 
 def test_filter_by_currency_case_sensitive():
-    result = list(filter_by_currency([
-        {"operationAmount": {"currency": {"code": "RUB"}}
-        }],
-        'rub'))
-    assert result == [{'operationAmount': {'currency': {'code': 'RUB'}}}]
+    result = list(filter_by_currency([{"operationAmount": {"currency": {"code": "RUB"}}}], "rub"))
+    assert result == [{"operationAmount": {"currency": {"code": "RUB"}}}]
 
 
 def test_filter_by_currency_no_matches():
-    result = list(filter_by_currency([
-        {"operationAmount": {"currency": {"code": "RUB"}}
-        },
-        {"operationAmount": {"currency": {"code": "USD"}}
-         }
-    ],
-        'EUR'))
+    result = list(
+        filter_by_currency(
+            [{"operationAmount": {"currency": {"code": "RUB"}}}, {"operationAmount": {"currency": {"code": "USD"}}}],
+            "EUR",
+        )
+    )
     assert result == []
 
 
 def test_filter_by_currency_empty_list():
-    result = list(filter_by_currency([], 'USD'))
+    result = list(filter_by_currency([], "USD"))
     assert result == []
 
 
 def test_transaction_descriptions_base(generators_data):
     gen = transaction_descriptions(generators_data)
-    assert next(gen) == 'Перевод организации'
-    assert next(gen) == 'Перевод со счета на счет'
-    assert next(gen) == 'Перевод со счета на счет'
-    assert next(gen) == 'Перевод с карты на карту'
-    assert next(gen) == 'Перевод организации'
+    assert next(gen) == "Перевод организации"
+    assert next(gen) == "Перевод со счета на счет"
+    assert next(gen) == "Перевод со счета на счет"
+    assert next(gen) == "Перевод с карты на карту"
+    assert next(gen) == "Перевод организации"
     with pytest.raises(StopIteration):
         next(gen)
 
 
-@pytest.mark.parametrize('transactions_info, expected',
-[
-    (
-        [
-            {"operationAmount": {"currency": {"code": "RUB"}},
-        "description": "Перевод организации"},
-        {"operationAmount": {"currency": {"code": "USD"}},
-        "description": "Перевод с карты на карту"},
-        {"operationAmount": {"currency": {"code": "EUR"}},
-        "description": "Анонимный перевод"}
-        ],
-        'Перевод организации, Перевод с карты на карту, Анонимный перевод'
-    )
-])
+@pytest.mark.parametrize(
+    "transactions_info, expected",
+    [
+        (
+            [
+                {"operationAmount": {"currency": {"code": "RUB"}}, "description": "Перевод организации"},
+                {"operationAmount": {"currency": {"code": "USD"}}, "description": "Перевод с карты на карту"},
+                {"operationAmount": {"currency": {"code": "EUR"}}, "description": "Анонимный перевод"},
+            ],
+            "Перевод организации, Перевод с карты на карту, Анонимный перевод",
+        )
+    ],
+)
 def test_transaction_descriptions_another_info(transactions_info, expected):
     gen = transaction_descriptions(transactions_info)
-    assert next(gen) == 'Перевод организации'
-    assert next(gen) == 'Перевод с карты на карту'
-    assert next(gen) == 'Анонимный перевод'
+    assert next(gen) == "Перевод организации"
+    assert next(gen) == "Перевод с карты на карту"
+    assert next(gen) == "Анонимный перевод"
     with pytest.raises(StopIteration):
         next(gen)
 
 
 def test_transaction_descriptions_empty_list():
     gen = transaction_descriptions([])
-    assert next(gen) == 'Нет данных'
+    assert next(gen) == "Нет данных"
     with pytest.raises(StopIteration):
         next(gen)
 
 
 def test_card_number_generator_base():
     gen = card_number_generator(1, 5)
-    assert next(gen) == '0000 0000 0000 0001'
-    assert next(gen) == '0000 0000 0000 0002'
-    assert next(gen) == '0000 0000 0000 0003'
-    assert next(gen) == '0000 0000 0000 0004'
-    assert next(gen) == '0000 0000 0000 0005'
+    assert next(gen) == "0000 0000 0000 0001"
+    assert next(gen) == "0000 0000 0000 0002"
+    assert next(gen) == "0000 0000 0000 0003"
+    assert next(gen) == "0000 0000 0000 0004"
+    assert next(gen) == "0000 0000 0000 0005"
     with pytest.raises(StopIteration):
         next(gen)
 
 
 def test_card_number_generator_another():
     gen = card_number_generator(77777, 77780)
-    assert next(gen) == '0000 0000 0007 7777'
-    assert next(gen) == '0000 0000 0007 7778'
-    assert next(gen) == '0000 0000 0007 7779'
-    assert next(gen) == '0000 0000 0007 7780'
+    assert next(gen) == "0000 0000 0007 7777"
+    assert next(gen) == "0000 0000 0007 7778"
+    assert next(gen) == "0000 0000 0007 7779"
+    assert next(gen) == "0000 0000 0007 7780"
     with pytest.raises(StopIteration):
         next(gen)
 
 
 def test_card_numbers_generator_invalid_parameters():
     gen = card_number_generator(33, 10)
-    assert next(gen) == f'Заданы неверные параметры start > stop: {33} > {10}'
+    assert next(gen) == f"Заданы неверные параметры start > stop: {33} > {10}"
     with pytest.raises(StopIteration):
         next(gen)
 
 
 def test_card_numbers_generator_invalid_parameter():
-    gen = card_number_generator(10, '22')
+    gen = card_number_generator(10, "22")
     with pytest.raises(TypeError):
         next(gen)
 
@@ -172,5 +171,3 @@ def test_card_numbers_generator_last_values():
     assert next(gen) == "Достигнуто крайнее значение: 9999 9999 9999 9999"
     with pytest.raises(StopIteration):
         next(gen)
-
-
