5065a0caeec74abdf019461019cd9bd726b79145
Vadim Semenov <vadimsemenov53@gmail.com>
2025-12-29 17:41:16 +0300

    clear code src/decorators -> black, isort, mypy, flake8

--- src/decorators.py
+++ src/decorators.py
@@ -1,40 +1,42 @@
-from time import time
-from functools import wraps
 import os
+from functools import wraps
+from time import time
+from typing import Any, Callable
 
 
-def log(filename = ''):
-    def wrapper(func):
+def log(filename: str = "") -> Callable[..., Any]:
+    def wrapper(func: Callable[..., Any]) -> Callable[..., Any]:
         @wraps(func)
-        def inner(*args, **kwargs):
+        def inner(*args: Any, **kwargs: Any) -> Any:
             start_time = time()
 
             try:
                 result = func(*args, **kwargs)
                 end_time = time()
-                func_data = f'''Функция: {func.__name__}, вызвана с аргументами {args}, и ключевыми аргументами {kwargs}.
+                func_data = f"""Функция: {func.__name__}.
+Вызвана с аргументами {args}, и ключевыми аргументами {kwargs}.
 Результат: {result}
-Время работы = {end_time - start_time:.6f}'''
+Время работы = {end_time - start_time:.6f}"""
                 flag = True
 
             except Exception as error:
                 end_time = time()
-                func_data = f'''Функция: {func.__name__}, вызвана с аргументами {args}, и ключевыми аргументами {kwargs}.
+                func_data = f"""Функция: {func.__name__}.
+Вызвана с аргументами {args}, и ключевыми аргументами {kwargs}.
 Тип ошибки: {type(error).__name__}
-Время до ошибки = {end_time - start_time:.6f}'''
+Время до ошибки = {end_time - start_time:.6f}"""
                 flag = False
-
 
             if filename:
                 if not isinstance(filename, str):
-                    raise TypeError('Имя файла должно быть строкой')
+                    raise TypeError("Имя файла должно быть строкой")
 
-                print(f'Данные записаны в файл -> {filename}')
+                print(f"Данные записаны в файл -> {filename}")
 
-                path_data = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+                path_data = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
                 path_full = os.path.join(path_data, filename)
-                with open(path_full, 'a', encoding="utf-8") as file:
-                    file.write(f'{func_data}\n')
+                with open(path_full, "a", encoding="utf-8") as file:
+                    file.write(f"{func_data}\n")
                     file.write(f"{'-' * 80}\n")
 
             else:
@@ -44,14 +46,7 @@ def log(filename = ''):
                 return func(*args, **kwargs)
             else:
                 raise
+
         return inner
+
     return wrapper
-
-
-
-@log()
-def example(*nums):
-    """Функция передачи данных"""
-    return nums
-
-gen = example([i for i in range(10)])
