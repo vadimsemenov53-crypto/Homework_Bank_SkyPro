04c0efc965784cba4957a43b21a9e74337f0d1da
Vadim Semenov <vadimsemenov53@gmail.com>
2026-01-25 18:54:33 +0300

    clear code utils

--- utils/utils.py
+++ utils/utils.py
@@ -2,14 +2,14 @@ import json
 from json import JSONDecodeError
 
 
-def get_operations(path_to_file: str)-> list[dict]:
+def get_operations(path_to_file: str) -> list[dict]:
     """Функция принимает путь до файла: {path} (файл с расширением 'json') с банковскими операция
     и возвращает список словарей банковских операций.
     Пример пути ->
-    '/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json' """
+    '/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json'"""
 
     try:
-        with open(path_to_file, encoding='utf-8') as operations_file:
+        with open(path_to_file, encoding="utf-8") as operations_file:
             try:
                 data_operations = json.load(operations_file)
             except JSONDecodeError:
@@ -24,6 +24,3 @@ def get_operations(path_to_file: str)-> list[dict]:
         return []
 
     return data_operations
-
-
-print(get_operations('/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json'))
