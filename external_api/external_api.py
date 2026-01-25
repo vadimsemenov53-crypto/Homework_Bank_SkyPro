1ab988a0435b6cf8fb4307927680330764622439
Vadim Semenov <vadimsemenov53@gmail.com>
2026-01-25 19:38:19 +0300

    install dotenv

--- external_api/external_api.py
+++ external_api/external_api.py
@@ -1,4 +1,28 @@
 import requests
+from dotenv import load_dotenv
 
-def get_amount_in_rubles():
+def get_amount_in_rubles(transaction: dict) -> float:
+    """Функция принимает на вход словарь с данными о транзакции.
+    Возвращает сумму транзакции в Рублях.
+    Если транзакция в иной валюте, то происходит обращение
+    к внешнему API для получения текущего курса валют
+    и конвертации суммы операции в рубли"""
     pass
+
+
+
+transaction_data = {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }
