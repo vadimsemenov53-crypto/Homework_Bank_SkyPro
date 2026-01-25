41ca3a13ef2607908d45493b263f8597ebb4ccd6
Vadim Semenov <vadimsemenov53@gmail.com>
2026-01-25 19:51:18 +0300

    add func get_amout_in_rubles for RUB

--- external_api/external_api.py
+++ external_api/external_api.py
@@ -1,13 +1,24 @@
 import requests
 from dotenv import load_dotenv
 
+load_dotenv('.env')
+
+API_KEY =
+
 def get_amount_in_rubles(transaction: dict) -> float:
     """Функция принимает на вход словарь с данными о транзакции.
     Возвращает сумму транзакции в Рублях.
     Если транзакция в иной валюте, то происходит обращение
     к внешнему API для получения текущего курса валют
     и конвертации суммы операции в рубли"""
-    pass
+    try:
+        if transaction['operationAmount']['currency']['code'] == 'RUB':
+            return transaction['operationAmount']['amount']
+        else:
+
+    except KeyError:
+        raise KeyError('Не найден ключ')
+    return 0
 
 
 
@@ -26,3 +37,4 @@ transaction_data = {
     "from": "Maestro 1596837868705199",
     "to": "Счет 64686473678894779589"
   }
+print(get_amount_in_rubles(transaction_data))
