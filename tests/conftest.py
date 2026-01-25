3483cbc4695c4c9f3a32e4c30825e0005e313c94
Vadim Semenov <vadimsemenov53@gmail.com>
2026-01-25 22:25:33 +0300

    testing utils.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -92,34 +92,48 @@ def generators_data():
 @pytest.fixture()
 def operations_data_fixture():
     return [
-  {
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-  },
-  {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
-  }
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589",
+        },
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "MasterCard 7158300734726758",
+            "to": "Счет 35383033474447895560",
+        },
     ]
+
+
+@pytest.fixture()
+def transaction_data_rub():
+    return {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {"amount": "1000", "currency": {"name": "рубль", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
+
+
+@pytest.fixture()
+def transaction_data_usd():
+    return {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {"amount": "1000", "currency": {"name": "usd", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    }
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,37 +1,39 @@
 import json
-from unittest.mock import Mock, patch
+from unittest.mock import patch
+
 from src.utils import get_operations
 
-@patch('src.utils.json.load')
+
+@patch("src.utils.json.load")
 def test_get_operations_base(mock_get, operations_data_fixture):
     mock_get.return_value = operations_data_fixture
 
-    result = get_operations('/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json')
+    result = get_operations("/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json")
     assert result == operations_data_fixture
 
 
-@patch('src.utils.json.load')
+@patch("src.utils.json.load")
 def test_get_operations_invalid_data(mock_get):
     data = []
     mock_get.return_value = data
-    result = get_operations('/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json')
+    result = get_operations("/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json")
     assert result == []
 
 
 def test_get_operations_file_not_found():
-    result = get_operations('/user/data.json')
+    result = get_operations("/user/data.json")
     assert result == []
 
 
-@patch('src.utils.json.load')
+@patch("src.utils.json.load")
 def test_get_operations_not_list(mock_get):
     mock_get.return_value = {"key": "value"}
-    result = get_operations('/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json')
+    result = get_operations("/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json")
     assert result == []
 
 
-@patch('src.utils.json.load')
+@patch("src.utils.json.load")
 def test_get_operations_invalid_json(mock_get):
     mock_get.side_effect = json.JSONDecodeError("errorJSONDecode", "", 0)
-    result = get_operations('/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json')
+    result = get_operations("/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/operations.json")
     assert result == []
