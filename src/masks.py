6617e5617645ef966e99fce45c5246e0f5712e46
Vadim Semenov <vadimsemenov53@gmail.com>
2026-01-30 11:41:51 +0300

    add logger for masks.py

--- src/masks.py
+++ src/masks.py
@@ -9,22 +9,37 @@ logger.addHandler(file_handler)
 
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует номер карты клиента"""
-    logger.info(f"Маскируем переданный номер карты: {card_number}")
-    card_mask = card_number[:4] + " " + card_number[4:6] + "**" + " " + "****" + " " + card_number[12:]
+    try:
+        logger.info(f"Маскируем переданный номер карты: {card_number}")
+        card_mask = card_number[:4] + " " + card_number[4:6] + "**" + " " + "****" + " " + card_number[12:]
 
-    logger.info(f"Выполняем проверку соответствия номера карты требованиям")
-    if len(card_number) < 16:
-        logger.warning(f"Переданный номер содержит: {len(card_number)} символов < 16")
-        return "Номер карты должен содержать 16 цифр"
-    elif len(card_number) > 16:
-        logger.warning(f'Переданный номер содержит: {len(card_number)} символов > 16')
-        return "Номер карты содержит более 16 цифр"
-    else:
-        logger.info('Успешная маскировка. Работа закончена.')
-        return card_mask
+        logger.info(f"Выполняем проверку соответствия номера карты требованиям")
+        if len(card_number) < 16:
+            logger.error(f"Переданный номер содержит: {len(card_number)} символов < 16")
+            return "Номер карты должен содержать 16 цифр"
+
+        elif len(card_number) > 16:
+            logger.error(f'Переданный номер содержит: {len(card_number)} символов > 16')
+            return "Номер карты содержит более 16 цифр"
+
+        else:
+            logger.info('Успешная маскировка. Работа закончена.')
+            return card_mask
+
+    except Exception as error:
+        logger.error(f"Произошла ошибка: {error}")
+        return f'Маскировка невозможна: {error}'
 
 
 def get_mask_account(numbers_account: str) -> str:
     """Функция маскирует номер счета"""
-    mask_account = "**" + numbers_account[-4:]
-    return mask_account
+    try:
+        logger.info(f'Принимаем для маскировки номер счета: {numbers_account}')
+        mask_account = "**" + numbers_account[-4:]
+
+        logger.info(f'Успешная маскировка. Работа закончена.')
+        return mask_account
+
+    except Exception as error:
+        logger.error(f"Произошла ошибка: {error}")
+        return f'Маскировка невозможна: {error}'
