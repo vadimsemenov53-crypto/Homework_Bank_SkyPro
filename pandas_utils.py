a3648757596c3acf1a89447d24278f6ce8ea1ad8
Vadim Semenov <vadimsemenov53@gmail.com>
2026-02-06 16:16:52 +0300

    add file .csv and.xlsx for work func

--- pandas_utils.py
+++ pandas_utils.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 def read_transactions_from_csv(path: str) -> list[dict]:
+    """Считывает финансовые операции из CSV-файла и возвращает список словарей."""
     ...
 
 def read_transactions_from_excel(path: str) -> list[dict]:
+    """Считывает финансовые операции из Excel-файла и возвращает список словарей."""
     ...
