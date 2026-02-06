ca6b6a70ff811b2cfa644a2970cc757466c157b7
Vadim Semenov <vadimsemenov53@gmail.com>
2026-02-06 16:43:40 +0300

    add func for read file CSV and XLSX

--- pandas_utils.py
+++ pandas_utils.py
@@ -1,21 +1,27 @@
 import pandas as pd
-import openpyxl
 
-def read_transactions_from_csv(path: str) -> list[dict]:
-    """Считывает финансовые операции из CSV-файла и возвращает список словарей."""
-    df = pd.read_csv(path)
-    return df
+def read_transactions_from_csv(path_to_file: str) -> list[dict]:
+    """Функция принимает путь до файла (path_to_file),
+    считывает финансовые операции из CSV-файла и возвращает список словарей.
+    Пример для передачи пути: '/Users/PycharmProjects/data/transactions.csv'."""
+    df = pd.read_csv(path_to_file)
+
+    list_data = df.to_dict(orient='records')
+    return list_data
 
 
-def read_transactions_from_excel(path: str) -> list[dict]:
-    """Считывает финансовые операции из Excel-файла и возвращает список словарей."""
-    df = pd.read_excel(path)
-    return df
+def read_transactions_from_excel(path_to_file: str) -> list[dict]:
+    """Функция принимает путь до файла (path_to_file),
+    считывает финансовые операции из Excel-файла и возвращает список словарей.
+    Пример для передачи пути: '/Users/PycharmProjects/data/transactions_excel.xlsx'."""
+    df = pd.read_excel(path_to_file)
+
+    list_data = df.to_dict(orient='records')
+    return list_data
 
 
 if __name__ == '__main__':
     path_csv = '/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/transactions.csv'
     path_xlsx = '/Users/vadimsemenov/PycharmProjects/Homework_Bank_SkyPro/data/transactions_excel.xlsx'
 
-    print(read_transactions_from_csv(path_csv))
     print(read_transactions_from_excel(path_xlsx))
