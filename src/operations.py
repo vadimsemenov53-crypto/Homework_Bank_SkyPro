7c41b94c582c9d732389469e29993d404c9a0ed8
Vadim Semenov <vadimsemenov53@gmail.com>
2026-02-10 21:25:01 +0300

    add func count_operations_by_category

--- src/operations.py
+++ src/operations.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 def filter_operations_by_description(data_operations: list[dict], search:str) -> list[dict]:
     """Функция принимает данные о банковских операциях (список словарей),
@@ -6,13 +7,26 @@ def filter_operations_by_description(data_operations: list[dict], search:str) ->
     pattern = re.compile(search, re.IGNORECASE)
 
     result_filter = []
-    for operations in data_operations:
+    for operation in data_operations:
         if search:
-            if pattern.findall(operations.get('description')):
-                result_filter.append(operations)
+            if pattern.findall(operation.get('description')):
+                result_filter.append(operation)
 
         else:
             return []
 
     return result_filter
 
+
+def count_operations_by_category(data_operations: list[dict], categories: list[str]) -> dict:
+    """Функция принимает данные об операциях и список категория,
+    возвращает словарь {название категорий -> количество операций}"""
+    counter = Counter()
+
+    for operation in data_operations:
+        description = operation['description']
+
+        if description in categories:
+            counter[description] += 1
+
+    return dict(counter)
