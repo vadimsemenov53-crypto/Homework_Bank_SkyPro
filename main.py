d83c5fe230e8d283733c6c12fc43769f549ff5a3
Vadim Semenov <vadimsemenov53@gmail.com>
2026-02-11 01:37:54 +0300

    version 1 - main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.utils import get_operations
 from pandas_utils import read_transactions_from_csv, read_transactions_from_excel
 from src.processing import filter_by_state, sort_by_date
+from src.operations import filter_operations_by_description
+from src.widget import mask_account_card
 
 VALID_STATUSES = ["EXECUTED", "CANCELED", "PENDING"]
 
-def main():
+def main() -> None | str:
     print("""Программа: Привет! Добро пожаловать в программу работы 
 с банковскими транзакциями. 
 Выберите необходимый пункт меню:
@@ -52,6 +54,7 @@ def main():
 
     print("Программа: Отсортировать операции по дате? Да/Нет")
     choice_date = str(input('Пользователь : ').upper().strip())
+
     if choice_date == 'ДА':
         print("Программа: Отсортировать по возрастанию? Да/Нет")
 
@@ -63,11 +66,39 @@ def main():
             result_filter = sort_by_date(result_filter, True)
 
     print("Программа: Выводить только рублевые транзакции? Да/Нет")
+    choice_currency = str(input('Пользователь : ')).upper().strip()
 
-    return result_filter
+    if choice_currency == 'ДА':
+        result_filter = [
+            trans for trans in result_filter
+            if trans["operationAmount"]["currency"]["code"] == 'RUB'
+        ]
+
+    print("Программа: Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
+    choice_description = str(input('Пользователь : ')).upper().strip()
+
+    if choice_description == 'ДА':
+        print("Программа: По какому слову в описании будем фильтровать?")
+        word_user = str(input('Пользователь : ')).strip()
+
+        result_filter = filter_operations_by_description(result_filter, word_user)
+
+    print(f"""Программа: Распечатываю итоговый список транзакций...
+Программа: Всего банковских операций в выборке: {len(result_filter)}""")
+
+    for transaction in result_filter:
+        if transaction['description'] == 'Открытие вклада':
+            print(f'{transaction['date'][:10]} {transaction['description']}')
+            print(f'{mask_account_card(transaction["to"])}')
+            print(f'Сумма: {transaction["operationAmount"]["amount"]} {transaction["operationAmount"]["currency"]["name"]}')
+        else:
+            print(f'{transaction['date'][:10]} {transaction['description']}')
+            print(f'{mask_account_card(transaction["from"])} -> {mask_account_card(transaction["to"])}')
+            print(f'Сумма: {transaction["operationAmount"]["amount"]} {transaction["operationAmount"]["currency"]["name"]}')
+
 
 
 
 
 if __name__ == '__main__':
-    print(main())
+    main()
